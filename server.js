fd27511926f2abbda1a562e631ab98faef7f4b02
202296 <mouhamedkoko996@gmail.com>
2023-06-23 22:41:47 +0000

    testing message

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const baseController = require("./controllers/baseController");
 const app = express();
 const utilities = require("./utilities/");
 const errorRoute = require("./routes/errorRoute");
+const session = require("express-session");
+const pool = require("./database/");
 
 
 
@@ -22,6 +24,27 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "./layouts/layout"); // not at views root
 
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
