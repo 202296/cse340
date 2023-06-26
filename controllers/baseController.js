d4187585a08cdc7ca4f621458cf1b3dae59a49c0
202296 <mouhamedkoko996@gmail.com>
2023-06-26 19:08:50 +0000

    updated the registration views

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,7 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  req.flash("notice", "Sorry, the registration failed.");
   res.render("index", {title: "Home", nav})
 }
 
