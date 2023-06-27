2dcf0dcd7f41068fe48af3793091955ffbb12d33
202296 <mouhamedkoko996@gmail.com>
2023-06-27 16:25:11 +0000

    update the account security

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,6 @@
-const utilities = require(".")
-const { body, validationResult } = require("express-validator")
+const utilities = require(".");
+const { body, validationResult } = require("express-validator");
+const accountModel = require("../models/account-model");
 const validate = {}
 
 /*  **********************************
@@ -19,12 +20,18 @@ validate.registationRules = () => {
         .isLength({ min: 2 })
         .withMessage("Please provide a last name."), // on error this message is sent.
   
-      // valid email is required and cannot already exist in the DB
+      // valid email is required and cannot already exist in the database
       body("account_email")
-      .trim()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+        .trim()
+        .isEmail()
+        .normalizeEmail() // refer to validator.js docs
+        .withMessage("A valid email is required.")
+        .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+            throw new Error("Email exists. Please log in or use different email")
+        }
+        }),
   
       // password is required and must be strong password
       body("account_password")
@@ -39,6 +46,33 @@ validate.registationRules = () => {
         .withMessage("Password does not meet requirements."),
     ]
   }
+
+
+/*  **********************************
+ *  Login Data Validation Rules
+ * ********************************* */
+validate.loginRules = () => {
+    return [
+    // valid email is required and cannot already exist in the database
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail() // refer to validator.js docs
+      .withMessage("A valid email is required."),
+
+    // password is required and must be strong password
+    body("account_password")
+      .trim()
+      .isStrongPassword({
+        minLength: 12,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+      .withMessage("Password does not meet requirements."),
+  ]
+}
 
 
 /* ******************************
@@ -56,6 +90,26 @@ validate.checkRegData = async (req, res, next) => {
         nav,
         account_firstname,
         account_lastname,
+        account_email,
+      })
+      return
+    }
+    next()
+  }
+
+/* ******************************
+ * Check data and return errors or continue to registration
+ * ***************************** */
+validate.checkLogData = async (req, res, next) => {
+    const { account_email } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      let nav = await utilities.getNav()
+      res.render("account/login", {
+        errors,
+        title: "Login",
+        nav,
         account_email,
       })
       return
