427c9b7c8b7dbae4ca35b139a049e7f7869b46b8
202296 <mouhamedkoko996@gmail.com>
2023-07-06 10:58:13 +0000

    upadated the model file

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -137,7 +137,7 @@ async function modifyInventory(
   inv_color,
   classification_id){
   try {
-    const sql = "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *"
+    const sql = "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_year = $3, inv_description = $4, inv_image = $5, inv_thumbnail = $6, inv_price = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *"
     const data = await pool.query(sql, [
       inv_make,
       inv_model,
